c1261b748ab665256c279a8112576b5c55e8b232
Shea Close <sheaclose@Sheas-MacBook-Pro.local>
2018-07-30 11:56:55 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { reactLocalStorage as ls } from "reactjs-localstorage";
 import "./App.css";
 
 class App extends Component {
@@ -12,14 +13,33 @@ class App extends Component {
   submit = e => {
     e.preventDefault();
     let { todos, userInput } = this.state;
+    let newTodos = [...todos, userInput];
     this.setState({
-      todos: [...todos, userInput],
+      todos: newTodos,
       userInput: ""
     });
+    ls.setObject("todos", newTodos);
   };
+  deleteTodo = i => {
+    let { todos } = this.state;
+    let copy = todos.slice();
+    copy.splice(i, 1);
+    this.setState({ todos: copy });
+    ls.setObject("todos", copy);
+  };
+  componentDidMount() {
+    let todos = ls.getObject("todos");
+    if (Array.isArray(todos)) {
+      this.setState({ todos });
+    }
+  }
 
   render() {
-    let todos = this.state.todos.map((todo, i) => <div key={i}>{todo}</div>);
+    let todos = this.state.todos.map((todo, i) => (
+      <div onClick={() => this.deleteTodo(i)} key={i}>
+        {todo}
+      </div>
+    ));
     return (
       <div className="App">
         <header className="App-header">
