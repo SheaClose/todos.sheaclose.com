0b08772cc46856adfe9f2510e380764dc5e3f554
Shea Close <sheaclose@Sheas-MacBook-Pro.local>
2018-07-30 10:15:23 -0500

    done

--- server/index.js
+++ server/index.js
@@ -5,6 +5,13 @@ const express = require("express"),
   app = express(),
   path = require("path");
 
+const { exec } = require("child_process");
+
+exec("npm run build", (err, stdout, stderr) => {
+  console.log("stderr: ", stderr);
+  console.log("stdout: ", stdout);
+  console.log("err: ", err);
+});
 app.use(cors());
 app.use(bodyParser.json());
 
