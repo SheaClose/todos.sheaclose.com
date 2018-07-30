de653c6434cc07d2e68bd5ede5154d81f6bd1568
Shea Close <sheaclose@Sheas-MacBook-Pro.local>
2018-07-30 09:42:46 -0500

    done

--- server/index.js
+++ server/index.js
@@ -10,9 +10,10 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname + "/../build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/../build/index.html"));
-// });
+app.get("*", (req, res) => {
+  console.log("landed");
+  res.sendFile(path.join(__dirname + "/../build/index.html"));
+});
 app.listen(port, () => {
   console.log("Server listening on port", port);
 });
