c47e21502b99b1db6e0909c9cdecedf5beb2f831
Shea Close <sheaclose@Sheas-MacBook-Pro.local>
2018-07-29 19:27:05 -0500

    server added

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express"),
+  cors = require("cors"),
+  bodyParser = require("body-parser"),
+  port = 8080,
+  app = express(),
+  path = require("path");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname + "/../build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/../build/index.html"));
+// });
+app.listen(port, () => {
+  console.log("Server listening on port", port);
+});
